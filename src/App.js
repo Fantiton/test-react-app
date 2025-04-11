6912a1e7749fc2c0102a487b929ea242397c8a1f
Fantiton <filipcelejewski@wp.pl>
2025-04-11 09:40:10 +0200

    Nowy ReactApp

--- src/App.js
+++ src/App.js
@@ -1,24 +1,19 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
+
+function RandomImage() {
+  const[url, setUrl] = useState("https://picsum.photos/200/300");
+  return(){
+    <img src={url} alt="Random" />
+  }
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>{}
+    <RandomImgage />
+    </>
   );
 }
 
