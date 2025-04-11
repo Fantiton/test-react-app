e2398a6aaf3a4f3a199d32e35782e2bd27aa5ed9
Fantiton <filipcelejewski@wp.pl>
2025-04-11 09:45:21 +0200

    function RandomImage()

--- src/App.js
+++ src/App.js
@@ -4,15 +4,18 @@ import { useState } from 'react';
 
 function RandomImage() {
   const[url, setUrl] = useState("https://picsum.photos/200/300");
+  function reload(){
+    setUrl("https://picsum.photos/200/300?random=" + Math.random());
+  }
   return(
-    <img src={url} alt="Random" />
+    <img src={url} onClick={reload} alt="Random" />
   )
 }
 
 function App() {
   return (
     <>{}
-    <RandomImgage />
+    <RandomImage />
     </>
   );
 }
